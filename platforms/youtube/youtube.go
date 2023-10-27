4f62754209ac754e86688def19f75532df879ee3
dohnj0e <dohnj0@proton.me>
2023-10-27 19:19:01 +0300

    Added scrolling to output more data, youtube

--- platforms/youtube/youtube.go
+++ platforms/youtube/youtube.go
@@ -5,6 +5,7 @@ import (
 	"net/url"
 	"os"
 	"strings"
+	"time"
 
 	"github.com/dohnj0e/snagtag/config"
 	"github.com/dohnj0e/snagtag/logger"
@@ -59,6 +60,110 @@ func InitWebDriver() (selenium.WebDriver, error) {
 	return wd, nil
 }
 
+func CheckPageLoadState(wd selenium.WebDriver) error {
+	state, err := wd.ExecuteScript("return document.readyState", nil)
+
+	if err != nil {
+		return err
+	}
+	if state != "complete" {
+		logger.Log.Errorln("Page is not loaded yet")
+	}
+	return nil
+}
+
+func ScrollIncrementally(wd selenium.WebDriver, amount int) error {
+	script := fmt.Sprintf("window.scroll(0, %d);", amount)
+	_, err := wd.ExecuteScript(script, nil)
+	return err
+}
+
+func ScrollAndScrape(wd selenium.WebDriver, keyword string) error {
+	existingTitles := map[string]bool{}
+	encodedKeyword := url.QueryEscape(keyword)
+	searchURL = fmt.Sprintf("https://www.youtube.com/results?search_query=%s", encodedKeyword)
+
+	const MaxIndex = 100
+
+	err := wd.Get(searchURL)
+	if err != nil {
+		return err
+	}
+
+	closeButton, err := wd.FindElement(selenium.ByCSSSelector, "button.yt-spec-button-shape-next.yt-spec-button-shape-next--filled.yt-spec-button-shape-next--mono.yt-spec-button-shape-next--size-m")
+	if err != nil {
+		logger.Log.Error("Failed to find the 'accept all' button: ", err)
+		return err
+	}
+
+	err = closeButton.Click()
+	if err != nil {
+		logger.Log.Error("Failed to click the 'accept all' button: ", err)
+		return err
+	}
+
+	for {
+		time.Sleep(1 * time.Second)
+
+		err = ScrollIncrementally(wd, 1000)
+		if err != nil {
+			return err
+		}
+
+		prevScrollPos, err := wd.ExecuteScript("return window.pageYOffset;", nil)
+		if err != nil {
+			return err
+		}
+
+		_, err = wd.ExecuteScript("window.scroll(0, 25000);", nil)
+		if err != nil {
+			logger.Log.Errorln("Failed to scroll: ", err)
+			return err
+		}
+
+		time.Sleep(1 * time.Second)
+
+		currScrollPos, err := wd.ExecuteScript("return window.pageYOffset;", nil)
+		if err != nil {
+			return err
+		}
+
+		if prevScrollPos == currScrollPos {
+			break
+		}
+
+		elements, err := wd.FindElements(selenium.ByCSSSelector, "a#video-title")
+		if err != nil {
+			logger.Log.Errorln("Failed to find elements: ", err)
+			return err
+		}
+
+		for index, element := range elements {
+			if index >= MaxIndex {
+				fmt.Printf("\n")
+				logger.Log.Info("Scrape completed successfully")
+				return nil
+			}
+
+			title, err := element.Text()
+			if err != nil {
+				logger.Log.Errorln("Failed to retrieve element text: ", err)
+				continue
+			}
+
+			if title != "" &&
+				!strings.Contains(title, "Mix") &&
+				!strings.Contains(title, "Playlist") &&
+				!strings.Contains(title, "Greatest Hits") &&
+				!existingTitles[title] {
+				fmt.Printf("%d: %s\n", index, title)
+				existingTitles[title] = true
+			}
+		}
+	}
+	return nil
+}
+
 func Scrape(keyword string) error {
 	encodedKeyword := url.QueryEscape(keyword)
 	searchURL = fmt.Sprintf("https://www.youtube.com/results?search_query=%s", encodedKeyword)
@@ -67,31 +172,14 @@ func Scrape(keyword string) error {
 	if err != nil {
 		return err
 	}
+
 	defer wd.Quit()
 	defer service.Stop()
 
-	err = wd.Get(searchURL)
+	err = ScrollAndScrape(wd, keyword)
 	if err != nil {
 		return err
 	}
-
-	elements, err := wd.FindElements(selenium.ByCSSSelector, "a#video-title")
-	if err != nil {
-		logger.Log.Errorln("Failed to find elements: ", err)
-	}
-
-	for index, element := range elements {
-		title, err := element.Text()
-		if err != nil {
-			logger.Log.Errorln("Failed to retrieve element text: ", err)
-		} else {
-			if title != "" && !strings.Contains(title, "Mix -") {
-				fmt.Printf("%d: %s\n", index, title)
-			}
-		}
-	}
-	fmt.Printf("\n")
-	logger.Log.Info("Scrape completed successfully")
 
 	return nil
 }
